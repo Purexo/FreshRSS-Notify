a3a639e38d5b184fe253a33fda6062fcd368b60a
Purexo <theotime93@yahoo.fr>
2017-03-03 19:05:02 +0100

    + EventsManager

---
+++ background/EventsManager.js
@@ -0,0 +1,153 @@
+const manager = (function () {
+  /**
+   * Simple Event
+   * have name and data
+   * 
+   * @class Event
+   */
+  class Event {
+    /**
+     * Creates an instance of Event.
+     * @param {string} name 
+     * @param {any} data 
+     * 
+     * @memberOf Event
+     */
+    constructor (name, data) {
+      this.name = name;
+      this.data = data;
+    }
+  }
+
+  /**
+   * Simple EventsManager
+   * 
+   * you can
+   * - addListener - need name and listener
+   * - fire - need name and callback
+   * - removeListener - need listener
+   * 
+   * @class EventsManager
+   */
+  class EventsManager {
+    /**
+     * Creates an instance of EventsManager.
+     * @param {any} options
+     * @param {boolean} options.debug [options.debug=false] - if debug true, fire will log event in console.log and print trace
+     * 
+     * @memberOf EventsManager
+     */
+    constructor({debug=false}={}) {
+      this._debug = debug;
+      this.clear();
+    }
+
+    /**
+     * Add a listener
+     * 
+     * @param {string} name 
+     * @param {Function} listener - take one arg of Event type (data when fired if defined is in it)
+     * @returns {any} listener - usefull for removeListener
+     * 
+     * @example
+     * manager.addListener('is-ready', ({name, data}) => console.log(`${name} fired with data :`, data))
+     * manager.addListener('is-ready', (event => console.log(event))
+     * 
+     * @memberOf EventsManager
+     */
+    addListener(name, listener) {
+      const dispatcher = this._getNameDispatcher(name);
+      dispatcher.push(listener);
+
+      return listener;
+    }
+
+    /**
+     * fire event, will call all listener associated with name given
+     * 
+     * @param {string} name 
+     * @param {any} data 
+     * 
+     * @memberOf EventsManager
+     */
+    fire(name, data) {
+      const event = new Event(name, data);
+      const dispatcher = this._getNameDispatcher(name);
+
+      this._debug && console.log(`event ${name} is fired with data :`, data) || console.trace();
+
+      dispatcher
+        .forEach(listener => typeof listener == 'function' ? listener(event) : event);
+    }
+
+    /**
+     * remove listener associated with nameand listener given
+     * 
+     * @param {any} name 
+     * @param {any} listener 
+     * 
+     * @memberOf EventsManager
+     */
+    removeListener(name, listener) {
+      let dispatcher = this._getNameDispatcher(name);
+
+      this._dispatchers[name] = dispatcher.filter(inListener => listener != inListener);
+    }
+
+    /**
+     * remove all listener associated with name given
+     * 
+     * @param {any} name 
+     * 
+     * @memberOf EventsManager
+     */
+    removeAllListener(name) {
+      this._initNameDispatcher(name);
+    }
+
+    /**
+     * remove all listener
+     * 
+     * @memberOf EventsManager
+     */
+    clear() {
+      this._dispatchers = {};
+    }
+
+    get debug() {
+      return this._debug;
+    }
+
+    set debug(debug) {
+      this._debug = debug;
+
+      return true;
+    }
+
+    /**
+     * @param {string} name 
+     * @returns 
+     * 
+     * @memberOf EventsManager
+     * @private
+     */
+    _initNameDispatcher(name) {
+      return this._dispatchers[name] = [];
+    }
+
+    /**
+     * @param {string} name 
+     * @returns
+     * 
+     * @memberOf EventsManager
+     * @private
+     */
+    _getNameDispatcher (name) {
+      return this._dispatchers[name] || this._initNameDispatcher();
+    }
+  }
+
+  return new EventsManager();
+})();
+
+console.log(manager);
