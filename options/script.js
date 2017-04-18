1498d657d4461523ce92cb37b8dd2fdc9f72f301
Purexo <contact@purexo.mom>
2017-04-18 23:13:01 +0200

    FF does not support ::after on input, so return to span indicator + some info in manifest + package.json, incomplete and false

--- options/script.js
+++ options/script.js
@@ -1,6 +1,8 @@
 $.ready(() => {
   const $input_refresh_time = $('#input-refresh-time');
+  const $indicateur_refresh_time = $('#input-refresh-time + span');
   $input_refresh_time.addEventListener('change', () => {
     $input_refresh_time.setAttribute('value', $input_refresh_time.value);
+    $indicateur_refresh_time.textContent = `${$input_refresh_time.value} min`
   })
 });
