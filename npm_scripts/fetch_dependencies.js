327006ac8fec05a86686ecb986cbf63fdfa0c522
Purexo <contact@purexo.mom>
2017-07-22 21:20:21 +0200

    move files config in package.json

--- npm_scripts/fetch_dependencies.js
+++ npm_scripts/fetch_dependencies.js
@@ -17,17 +17,19 @@ function cp(src, target) {
   read.pipe(write);
 }
 
-const files = [
-  [
-    '../node_modules/webextension-polyfill/dist/browser-polyfill.js',
-    '../src/back/lib/from_npm/browser-polyfill.js',
-  ],
-  [
-    '../node_modules/i18next/dist/umd/i18next.js',
-    '../src/back/lib/from_npm/i18next.js',
-  ],
-];
+const npm_config_files = Object.getOwnPropertyNames(process.env)
+  .filter(env => env.startsWith('npm_package_config_files')) // [[1, 2], [1, 2], ...] struct
+  .map(env => process.env[env]) // get value env npm_package_config_files_*_*
+  .reduce((prev, curr, index) => { // fetch them in [[1, 2], [1, 2], ...] struct
+    if (index % 2 == 0) {
+      prev.push([curr]);
+    } else {
+      prev[prev.length - 1].push(curr)
+    }
+  
+    return prev;
+  }, []);
 
-files
-  .map(([src, target]) => [path.join(__dirname, src), path.join(__dirname, target)])
+npm_config_files
+  .map(([src, target]) => [path.join(src), path.join(target)])
   .forEach(([src, target]) => cp(src, target));
