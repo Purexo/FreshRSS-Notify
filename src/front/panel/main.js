3af9a39dc5ebb13e901a8e853f0d32e24121ba2c
Purexo <contact@purexo.mom>
2017-10-01 11:07:59 +0200

    + prefix id template with `tpl-`

--- src/front/panel/main.js
+++ src/front/panel/main.js
@@ -2,7 +2,7 @@ $(function () {
   const $container = $('.rss-item-container .mCSB_container');
   
   for (let i = 0; i < 5; i++) {
-    const rss_item = $(getTemplate('rss-item'));
+    const rss_item = $(getTemplate('tpl-rss-item'));
     $container.append(rss_item);
   }
 });
